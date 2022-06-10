31325f01bd8b38dedb049332f49f3966e8e96eef
iamshs <mdsolayman998@gmail.com>
2022-06-10 13:46:49 +0600

    setting up site link

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Projects from '../Projects/Projects';
+import myProjects from '../../projects';
 
 const Home = () => {
     return (
         <div className="">
           <Banner></Banner> 
-          <Projects></Projects> 
+          <div>
+          <div>
+          <h1 className="text-3xl text-center font-extrabold  my-5 font-serif">My Projects</h1>
+          </div>
+       <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+         {
+            myProjects.map((project,i)=><Projects
+            key={i}
+            project={project}
+            ></Projects>)
+          }
+         </div>
+          </div> 
         </div>
     );
 };
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,109 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Projects = () => {
+const Projects = ({project}) => {
+  const {live,git,name}= project
+
+  const liveSite =()=>{
+    <Link to={window.open(live)}>Live</Link>
+  }
+  const gitSite =()=>{
+    <Link to={window.open(git)}>Live</Link>
+  }
+
   return (
-    <div className="my-2">
-        <h1 className="text-3xl text-center font-extrabold  my-5 font-serif">My Projects</h1>
-    <div className="grid grid-cols-1 lg:grid-cols-3 lg:ml-40 ml-8 gap-2">
-    <div class="card max-w-xs bg-purple-300 shadow-xl border ">
-  <div class="card-body  ">
-    <h2 class="card-title">Wild Life</h2>
-    <div class="card-actions ">
-      <button class="btn btn-primary btn-xs">
-      <a
-          class='social-icon-link github'
-          href="https://github.com/iamshs/independence-service-provider-website"
-          target='_blank'
-          rel="noopener"
-          aria-label='Github'
-        >
-         Git Repo
-        </a>
-      </button>
+   
+  <>
+  
+  <div class="card max-w-xs bg-gradient-to-r from-purple-700 to-purple-500  shadow-xl ml-10 lg:ml-16">
+  {/* <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure> */}
+  <div class="card-body">
+    <h2 class=" text-3xl font-bold text-white text-center">
+     {name.toString()}
      
-      <button class="btn btn-primary btn-xs">
-      <a
-          class='social-icon-link github'
-          href="https://wild-life-b3cdd.web.app/"
-          target='_blank'
-          rel="noopener"
-          aria-label='Github'
-        >
-         Live Site
-        </a>
+    </h2>
+    
+    <div class="card-actions justify-center items-end">
+      <button className="btn btn-xs btn-primary " onClick={liveSite}>
+      Live Demo
       </button>
-     
-
+      <button className="btn btn-xs btn-primary" onClick={gitSite}>
+      Git Repo
+      </button>
     </div>
   </div>
 </div>
-    <div class="card max-w-xs bg-purple-300 shadow-xl border ">
-  <div class="card-body  ">
-    <h2 class="card-title">De Walt</h2>
-    <div class="card-actions ">
-      <button class="btn btn-primary btn-xs">
-      <a
-          class='social-icon-link github'
-          href="https://github.com/iamshs/manufacturer-website-client"
-          target='_blank'
-          rel="noopener"
-          aria-label='Github'
-        >
-         Git Repo
-        </a>
-      </button>
-     
-      <button class="btn btn-primary btn-xs">
-      <a
-          class='social-icon-link github'
-          href="https://de-walt-e12dc.web.app/"
-          target='_blank'
-          rel="noopener"
-          aria-label='Github'
-        >
-         Live Site
-        </a>
-      </button>
-     
-
-    </div>
-  </div>
-</div>
-    <div class="card max-w-xs bg-purple-300 shadow-xl border ">
-  <div class="card-body  ">
-    <h2 class="card-title">Motor House</h2>
-    <div class="card-actions ">
-      <button class="btn btn-primary btn-xs">
-      <a
-          class='social-icon-link github'
-          href="https://github.com/iamshs/warehouse-management-client"
-          target='_blank'
-          rel="noopener"
-          aria-label='Github'
-        >
-         Git Repo
-        </a>
-      </button>
-     
-      <button class="btn btn-primary btn-xs">
-      <a
-          class='social-icon-link github'
-          href="https://warhouse-management.web.app/"
-          target='_blank'
-          rel="noopener"
-          aria-label='Github'
-        >
-         Live Site
-        </a>
-      </button>
-     
-
-    </div>
-  </div>
-</div>
-    </div>
-    </div>
+  
+  </>
+    
+    
   );
 };
 
